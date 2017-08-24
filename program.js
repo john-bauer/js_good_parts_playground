ba8e2d404355fac4f686d6cd310987e14306d893
john bauer <johnbauer12693@gmail.com>
2017-08-24 17:06:08 -0400

    added reserved words

--- program.js
+++ program.js
@@ -1,8 +1,67 @@
 /* block comments can go on multiple lines BUT THEY SHOULD BE AVOIDED,
-because sometimes this syntax appears in regex literals */
+because sometimes this syntax appears in regex literals. I'll use them 
+in this only for the sake of readability. */
 
-// inline comments can not. They are safer.
+// inline comments are safer.
 
 const that = this;
 console.log(that);
 
+
+/*
+------- RESERVED WORDS ----------
+abstract
+boolean
+break
+byte
+case
+catch
+char
+class
+constcontinue
+debugger
+default
+delete
+dondouble
+else
+enum
+export
+extends
+false
+final
+finally
+float
+for
+var
+function
+goto
+if
+implements
+import
+ininstanceof
+int
+interface
+long
+native
+var null = new type(arguments);package
+private
+protected
+public
+return
+short
+static
+super
+switch
+synchronized
+thisthrow
+throws
+transient
+true
+try
+typeof
+var
+volatile
+void
+while
+with
+*/
