11f422479da51eec4c8b674b5e0482117c9637a7
john bauer <johnbauer12693@gmail.com>
2017-08-24 16:48:13 -0400

    starting grammar section

---
+++ program.js
@@ -0,0 +1,8 @@
+/* block comments can go on multiple lines BUT THEY SHOULD BE AVOIDED,
+because sometimes this syntax appears in regex literals */
+
+// inline comments can not. They are safer.
+
+const that = this;
+console.log(that);
+
